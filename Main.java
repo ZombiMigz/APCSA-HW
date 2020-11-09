e1bba586326afac783b6ba77922a71711e312880
John Kim <jkim@rowlandschools.org>
2020-11-08 20:30:06 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String[] sArr = {"hello", "world", "Mike", "Mechanics", "is", "Fungal", "OedipusComplex", "Rafael", "Leonardo"};
+
+        // print the shortest String
+
+        // Using a for-each loop, find the longest String
+
+        // Use a for loop to determine whether the word "Donnatello" is in the array?  Print "yes" or "no"
+
+           
+    }
+}
